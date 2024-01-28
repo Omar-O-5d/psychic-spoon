9a0c0825c8997c27ad4f413917fde993e4a802d6
Omar-O-5d <137425794+Omar-O-5d@users.noreply.github.com>
2024-01-28 21:30:14 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+concat() {
+    var first_name= "omar"
+    var second_name= "omar"
+    first_name.concat(l_name);
+    second_name.concat(l_name);
+    console.log();
+    const myObj = {firstname:"John", lastname:"Doe"};
+console.log(myObj);
+}
+
